73a7f4c9b7def25d9b9a1c54d2e8288b48b57455
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-20 20:43:13 +0100

    chore(ColorPaletteDetails.jsx): reformat code to improve readability and maintain consistency in code style chore(Navbar.jsx): remove unnecessary margin from the "New" button in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
 
   return (
     <div className="bg-bggrey">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6  flex justify-between items-center h-16">
         <Link to="/">
           <div className="text-2xl font-bold text-neutral">HUETOPIA</div>
         </Link>
@@ -28,7 +28,7 @@ export const Navbar = () => {
             ALL COLORS
           </NavLink>
           <Link to="/">
-            <button className="btn btn-neutral btn-sm m-5">New</button>
+            <button className="btn btn-neutral btn-sm ">New</button>
           </Link>
         </div>
         {/* Burger menu button positioned in top-right corner */}
