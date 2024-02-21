c940e07fc35a37946ecdaa9368f6fa51103f9050
Laia Navalon <laianavalon@gmail.com>
2024-02-21 18:20:27 +0100

    Change search input for DaisyUI component and move it

--- src/components/ColorPalettesList.jsx
+++ src/components/ColorPalettesList.jsx
@@ -44,22 +44,19 @@ function ColorPalettesList(props) {
 
   return (
     <div>
-      <label>Search:</label>
-      <input
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-        type="text"
-        value={value}
-        placeholder="enter your search"
-      />
       <div className="w-11/12">
-        {props.url === "favourites" && (
-          <h1 className="font-bold text-neutral">FAVOURITES</h1>
-        )}
-        {props.url === "palettes" && (
-          <h1 className="font-bold text-neutral">ALL COLORS</h1>
-        )}
+        <div className="inline flex justify-between items-center">
+          {props.url === "favourites" && (
+            <h1 className="font-bold text-neutral">FAVOURITES</h1>
+          )}
+          {props.url === "palettes" && (
+            <h1 className="font-bold text-neutral">ALL COLORS</h1>
+          )}
+          <label className="input input-bordered flex items-center gap-2 w-50 h-7 my-2">
+            <input value={value} type="text" className="grow" placeholder="Search" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+          </label>
+        </div>
         <hr className="border-neutral" />
       </div>
       <div className="flex flex-wrap text-neutral">
