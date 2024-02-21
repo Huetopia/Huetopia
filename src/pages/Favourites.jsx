08637f388c776971142d94813b3deef13935424a
Laia Navalon <laianavalon@gmail.com>
2024-02-21 15:57:32 +0100

    Add deployed project url to README.md + Add min vh to Favourites.jsx

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -2,7 +2,7 @@ import ColorPalettesList from "../components/ColorPalettesList";
 
 export const Favourites = (props) => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center min-h-vh">
       <ColorPalettesList url={props.url} />
     </div>
   );
