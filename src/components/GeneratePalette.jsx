f58cadffb00ca57065177f623a8df82e8f1e264f
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-21 11:54:41 +0100

    feat(netlify.toml): add netlify.toml configuration file to specify node_bundler as "esbuild" for Netlify functions feat(api-request-with-credentials.js): create a Netlify function to handle API requests with credentials to OpenAI API, generating color palettes based on user input feat(GeneratePalette.jsx): update handleSubmit function to make a GET request to the Netlify function api-request-with-credentials to generate color palettes

--- src/components/GeneratePalette.jsx
+++ src/components/GeneratePalette.jsx
@@ -5,103 +5,21 @@ import { useNavigate } from "react-router-dom";
 import { Button, Input } from "react-daisyui";
 
 export const GeneratePalette = () => {
-  const baseURL = "https://api.openai.com";
+ 
   const [theme, setTheme] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await axios.post(
-        baseURL + "/v1/chat/completions",
-        {
-          model: "gpt-3.5-turbo",
-          response_format: { type: "json_object" },
-          messages: [
-            {
-              role: "system",
-              content: `You are an assistant tasked with generating color palettes based on user input. Users provide you with a theme, which could be a single word or a description of a scene, along with multiple aspects to consider. You're responsible for finding 5 colors that fit those aspects. Your response should be in JSON format, structured as follows:
-      
-                    {
-                      "theme": {
-                        "input": "user's input",
-                        "emojis": ["emoji1", "emoji2", "emoji3"]
-                      },
-                      "colors": [
-                        {
-                          "hex": "#xxxxxx",
-                          "rgb": "rgb(x, x, x)",
-                          "description": "Description of what the color represents",
-                          "name": "Color name"
-                        },
-                        {
-                          "hex": "#xxxxxx",
-                          "rgb": "rgb(x, x, x)",
-                          "description": "Description of what the color represents",
-                          "name": "Color name"
-                        },
-                        {
-                          "hex": "#xxxxxx",
-                          "rgb": "rgb(x, x, x)",
-                          "description": "Description of what the color represents",
-                          "name": "Color name"
-                        },
-                        {
-                          "hex": "#xxxxxx",
-                          "rgb": "rgb(x, x, x)",
-                          "description": "Description of what the color represents",
-                          "name": "Color name"
-                        },
-                        {
-                          "hex": "#xxxxxx",
-                          "rgb": "rgb(x, x, x)",
-                          "description": "Description of what the color represents",
-                          "name": "Color name"
-                        }
-                      ]
-                    }`,
-            },
-            {
-              role: "user",
-              content: theme,
-            },
-          ],
-          temperature: 1,
-          max_tokens: 850,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      // define incoming Data
-      const newPalette = JSON.parse(response.data.choices[0].message.content);
-
-      console.log("Usage:", response.data.usage.total_tokens, "tokens");
-      console.log(response.data.choices[0].message.content);
-
-      // write to palettes API endpoint
-      axios
-        .post("https://huetopia-api.adaptable.app/palettes", newPalette)
-        .then((res) => {
-          console.log("Success writing to API Endpoint:", res);
-          // reset input
-          setTheme("");
-          navigate(`/palettes/${res.data.id}`);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    } catch (error) {
-      console.error("Error during API call: ", error);
-    }
+    axios.get(`https://huetopia.netlify.app/.netlify/functions/api-request-with-credentials`)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch( e => {
+      console.log(e)
+    })
+    
   };
 
   return (
@@ -115,7 +33,7 @@ export const GeneratePalette = () => {
           className="input input-bordered w-2/3 max-w-xs min-h-12 m-5"
           type="text"
           id="message"
-          value={theme}
+          // value={theme}
           placeholder="What's in your mind?"
           required
           onChange={(e) => setTheme(e.target.value)}
