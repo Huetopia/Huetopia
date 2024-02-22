7e2ac7f8f956a68ca812116daa0a102c5f749952
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-22 14:02:01 +0100

    fix(GeneratePalette.jsx): add loading state and error handling to form submission feat(GeneratePalette.jsx): display loading message and progress bar while AI is searching for inspiration

--- src/components/GeneratePalette.jsx
+++ src/components/GeneratePalette.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
 import example1 from "../assets/palette-example-1.png";
 
 export const GeneratePalette = () => {
   const [theme, setTheme] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
     try {
       // call netlify api that calls the OpenAi API internally
@@ -30,14 +35,21 @@ export const GeneratePalette = () => {
       navigate(`/palettes/${res2.data.id}`);
     } catch (error) {
       console.log(error);
+      setError(error);
+      toast.error("Something went wrong");
+      setIsLoading(false);
     }
   };
+
+ 
 
   return (
     <div className="w-full flex-grow flex  pt-20 bg-home bg-left-top bg-no-repeat bg-cover bg-opacity-20 bg-neutral-50 md:items-center md:pt-0">
       <div className="max-w-7xl mx-auto px-4 flex flex-col-reverse items-center h-full md:flex-row sm:px-6 lg:px-8 md:h-full">
         <div className="flex flex-col items-center md:items-start justify-center ">
-          <h2 className="text-5xl text-center md:text-left w-full font-bold mb-4 lg:w-3/4 ">Generate stunning color palettes with a tap.</h2>
+          <h2 className="text-5xl text-center md:text-left w-full font-bold mb-4 lg:w-3/4 ">
+            Generate stunning color palettes with a tap.
+          </h2>
           <p className="text-lg text-neutral mb-6 text-center md:text-left">
             Harness the power of AI to find color combinations that inspire your
             next project.
@@ -46,38 +58,34 @@ export const GeneratePalette = () => {
             onSubmit={handleSubmit}
             className="rounded-xl  flex flex-row justify-center md:justify-start items-start w-full"
           >
-            {/* <label className="font-bold mt-5 text-xl text-drk-grey flex flex-col items-center">
-              <p>Create your own AI</p>
-              <p>color palette</p>
-            </label> */}
-            <input
-              className="input input-bordered w-2/3  min-h-12  "
-              type="text"
-              id="message"
-              value={theme}
-              placeholder="Try a color, mood, or object..."
-              required
-              onChange={(e) => setTheme(e.target.value)}
-            />
-            <button className="btn btn-neutral ml-3" type="submit">
-              Inspire Me!
-            </button>
+            {!isLoading ? (
+              <>
+                <input
+                  className="input input-bordered w-2/3  min-h-12  "
+                  type="text"
+                  id="message"
+                  value={theme}
+                  placeholder="Try a color, mood, or object..."
+                  required
+                  onChange={(e) => setTheme(e.target.value)}
+                  disabled={isLoading}
+                />
+                <button className="btn btn-neutral ml-3" type="submit">
+                  Inspire Me!
+                </button>{" "}
+              </>
+            ) : (
+              <div className="mt-2">
+                <p className="text-neutral pb-3">AI is searching the rainbow for inspiration... 🌈</p>
+                <progress className="progress w-56"></progress>
+              </div>
+            )}
           </form>
         </div>
         <div className="stack w-2/4 mb-14 mt-10 sm:w-3/5 md:inline-grid lg:w-2/5">
-          <img
-            src={example1}
-            className="rounded"
-          />
-          <img
-            src={example1}
-            className="rounded"
-          />
-          <img
-            src={example1}
-            className="rounded"
-          />
-         
+          <img src={example1} className="rounded" />
+          <img src={example1} className="rounded" />
+          <img src={example1} className="rounded" />
         </div>
       </div>
       {/* <ColorPaletteDetails url={palette} /> */}
