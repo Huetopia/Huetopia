717897127578d42f9c225a98048fee87122adfa4
EazyErik <erik_busch@gmx.de>
2024-02-22 11:25:38 +0100

    add errorPage

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ import { Favourites } from "./pages/Favourites";
 import { About } from "./pages/About";
 import "tailwindcss/tailwind.css";
 import "daisyui/dist/full.css";
-import { ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ErrorPage } from "./pages/ErrorPage";
 
 // src/components
 import ColorPalettesList from "./components/ColorPalettesList";
@@ -26,12 +27,19 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<GeneratePalette />} />
-        <Route path="/palettes/:paletteId" element={<ColorPaletteDetails url="palettes"/>} />
+        <Route
+          path="/palettes/:paletteId"
+          element={<ColorPaletteDetails url="palettes" />}
+        />
         <Route path="/palettes" element={<AllColors url="palettes" />} />
         <Route path="/favourites" element={<Favourites url="favourites" />} />
-        <Route path="/favourites/:paletteId" element={<ColorPaletteDetails url="favourites"/>} />
+        <Route
+          path="/favourites/:paletteId"
+          element={<ColorPaletteDetails url="favourites" />}
+        />
         <Route path="/random" element={<RandomPalette />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
       <ToastContainer />
       <Footer />
