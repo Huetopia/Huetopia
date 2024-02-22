91e7eb6ba368f2bd388e9c21e9fa127673ec791b
Laia Navalon <laianavalon@gmail.com>
2024-02-22 11:21:28 +0100

    Add div for Emoji menu to ColorPalettesList.jsx

--- src/components/ColorPalettesList.jsx
+++ src/components/ColorPalettesList.jsx
@@ -37,32 +37,33 @@ function ColorPalettesList(props) {
           {props.url === "palettes" && (
             <h1 className="font-bold text-neutral">ALL COLORS</h1>
           )}
-          <div>
-            <FilterByEmoji url={props.url}/>
-            <label className="input input-bordered flex items-center gap-2 w-50 h-7 my-1">
-              <input
-                value={value}
-                type="text"
-                className="grow"
-                placeholder="Search"
-                onChange={(e) => setValue(e.target.value)}
+          <div className="flex items-end justify-between">
+            <h3 className="font-bold text-neutral text-sm mx-5">FILTER BY EMOJI</h3>
+          <label className="input input-bordered flex items-center gap-2 w-50 h-7 my-1">
+            <input
+              value={value}
+              type="text"
+              className="grow"
+              placeholder="Search"
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-4 h-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </label>
+            </svg>
+          </label>
           </div>
         </div>
-        <div className="divider mt-0"></div>
+        <div className="divider my-0"></div>
+        <FilterByEmoji url={props.url}/>
       </div>
       <div className="flex flex-wrap grid-cols-3 text-neutral ">
         {palettes === null ? (
